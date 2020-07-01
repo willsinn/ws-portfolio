62622ba6f377a00b627692f0cd20267d8b8dfe0b
William Sinn <w.h.sinn@gmail.com>
2020-07-01 03:17:37 -0400

    finish navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,20 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar-btn">
-        <span className="navbar-name">William Sinn</span>
+        <Link className="active-link" to="/about">
+          William Sinn
+        </Link>
       </div>
       <div className="horz-wrap">
         <div className="navbar-btn">
-          <Link to="/about">about</Link>
+          <Link className="active-link" to="/about">
+            about
+          </Link>
         </div>
         <div className="navbar-btn">
-          <Link to="/projects">projects</Link>
+          <Link className="active-link" to="/projects">
+            projects
+          </Link>
         </div>
       </div>
     </div>
