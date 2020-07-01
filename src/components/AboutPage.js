f65e38ccdf873e0e27a765c3dd4c5f2444d42161
William Sinn <w.h.sinn@gmail.com>
2020-07-01 03:30:35 -0400

    add elems & classes & styles

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const AboutPage = () => {
-  return <div className="about">About pgs</div>;
+  return (
+    <div className="about">
+      <div className="top-section">
+        <div className="picture-cont">
+          <div className="picture"></div>
+        </div>
+        <div className="summary-cont">
+          <p className="summary-text">
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English.
+          </p>
+        </div>
+      </div>
+      About pgs
+    </div>
+  );
 };
 
 export default AboutPage;
