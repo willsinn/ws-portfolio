371e62542420b10e2753ad24efdc4772d2ad183e
William Sinn <w.h.sinn@gmail.com>
2020-07-01 02:06:56 -0400

    add router & set-up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Switch, Router } from "react-router-dom";
 
 const App = () => {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/about" />
+        </Switch>
+      </Router>
+      hi
+    </div>
+  );
 };
 
 export default App;
