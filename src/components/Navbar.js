6bd09ec1640e552d49b58677451ab195b24107ee
William Sinn <w.h.sinn@gmail.com>
2020-07-01 02:41:25 -0400

    add styles, add elems & static html values

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,19 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar">
+      <div className="navbar-btn">
+        <span className="navbar-name">William Sinn</span>
+      </div>
       <ul>
         <li>
-          <Link to="/about">about</Link>
+          <div className="navbar-btn">
+            <Link to="/about">about</Link>
+          </div>
         </li>
         <li>
-          <Link to="/projects">projects</Link>
+          <div className="navbar-btn">
+            <Link to="/projects">projects</Link>
+          </div>
         </li>
       </ul>
     </div>
