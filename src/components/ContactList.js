4621b167bbbb904d2e813e092708320b122c6c2f
William Sinn <w.h.sinn@gmail.com>
2020-07-01 08:13:34 -0400

    finish footer & add render contact icons only

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -2,15 +2,27 @@ import React from "react";
 import Contact from "./Contact";
 import { contactMethods } from "../utils/static";
 
-const ContactList = () => {
+const ContactList = ({ orient }) => {
   const renderContacts = () => {
     const contacts = contactMethods();
     if (contacts && contacts.length > 0) {
       return contacts.map((contact) => (
-        <Contact key={`${contact.method}-id`} contact={contact} />
+        <Contact
+          key={`${contact.method}-id`}
+          contact={contact}
+          orient={orient}
+        />
       ));
     }
   };
-  return <ul className="col-1">{renderContacts()}</ul>;
+  return (
+    <>
+      {orient === "vertical" ? (
+        <ul className="col-1">{renderContacts()}</ul>
+      ) : (
+        <ul style={{ display: "flex" }}>{renderContacts()}</ul>
+      )}
+    </>
+  );
 };
 export default ContactList;
