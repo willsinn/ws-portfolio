36852502127da96df7a48a6f233c51b630f4e6ed
William Sinn <w.h.sinn@gmail.com>
2020-07-01 03:58:19 -0400

    refactor element structure & classnames, refactor text

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 const AboutPage = () => {
+  const renderSlogan = () => {
+    return "Short slogan, brand.";
+  };
   return (
     <div className="about">
       <div className="top-section">
         <div className="picture-cont">
           <div className="picture"></div>
         </div>
-        <div className="summary-cont">
-          <p className="summary-text">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout.
-          </p>
+        <div className="slogan-cont">
+          <p className="slogan-text">{renderSlogan()}</p>
         </div>
       </div>
       About pgs
