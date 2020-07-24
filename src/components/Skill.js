2200e29cad273710ac6a4c519df709e418fbd16f
William Sinn <w.h.sinn@gmail.com>
2020-07-24 19:02:13 -0400

    add skill icon element, renders to dom

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Sqlite from "../images/sqlite.png";
 
 const Skill = ({ skill }) => {
   return (
     <li key={`${skill.name}-id`} className="stack-list-item">
-      <div className="skill-cont">{skill.name}</div>
+      <div className="skill-cont">
+        {skill.name}
+        <div
+          className="skill-icon"
+          style={{ backgroundImage: `url(${Sqlite})` }}
+        />
+      </div>
     </li>
   );
 };
