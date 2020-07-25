8067890ee6a193fbfe4ba5bbafda73d307ad7e80
William Sinn <w.h.sinn@gmail.com>
2020-07-24 20:45:34 -0400

    remove excess elems, update nth(odd) project item to stagger pics

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -8,15 +8,13 @@ const ProjectsPage = () => {
     if (projects && projects.length > 0) {
       return projects.map((project) => (
         <li key={`${project.title}-id`} className="project-item">
-          <div className="horz-wrap">
-            <div className="prj-i-left col-1">{project.img}</div>
-            <div className="prj-i-right col-1">
-              <div className="prj-title">{project.title}</div>
-              <a href="/link" className="prj-link">
-                {project.link}
-              </a>
-              <div className="prj-desc">{project.desc}</div>
-            </div>
+          <div className="prj-i-left col-1">{project.img}</div>
+          <div className="prj-i-right col-1">
+            <div className="prj-title">{project.title}</div>
+            <a href="/link" className="prj-link">
+              {project.link}
+            </a>
+            <div className="prj-desc">{project.desc}</div>
           </div>
         </li>
       ));
