4aa714432c2bc476a301038e3fee1c9486d234fb
William Sinn <w.h.sinn@gmail.com>
2020-07-25 00:32:50 -0400

    finish project descriptions & project styling

--- src/utils/static.js
+++ src/utils/static.js
@@ -32,21 +32,21 @@ export const projectList = () => {
       link: "https://www.mywellotask.com",
       img: "snapshot.img",
       desc:
-        "Use to manage, track and organize todos, lists and long term goals.",
+        "A web app to help you manage, track and organize tasks and goals, using labels, checklists, lists and boards.",
     },
     {
       title: "Bus Stuff",
       link: "https://bus-stuff.herokuapp.com/",
       img: "snapshot.img",
       desc:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+        "A web app to help you compare coach bus ticket avaliability and prices. This includes saving favorites as well as direct purchase links.",
     },
     {
       title: "Zombie Diner",
       link: "https://zombie-diner.herokuapp.com/",
       img: "snapshot.img",
       desc:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+        "A phone app where you match recipes to complete all orders before the time runs out.",
     },
   ];
   return arr;
