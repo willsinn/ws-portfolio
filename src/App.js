b3e025a3d05d17d7c497d98de35e784fbb65f232
William Sinn <w.h.sinn@gmail.com>
2020-08-02 10:14:18 -0400

    finish landing layout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import AboutPage from "./components/AboutPage";
 import ProjectsPage from "./components/ProjectsPage";
 import LandingPage from "./components/LandingPage";
+import Navbar from "./components/Navbar";
 
 import "./App.css";
 
 const App = () => {
   return (
     <div className="app">
+      <Navbar />
+
       <LandingPage />
       <AboutPage />
       <ProjectsPage />
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,15 +1,23 @@
 import React from "react";
-import Navbar from "./Navbar";
 
 const LandingPage = () => {
   return (
-    <div className="landing">
-      <Navbar />
-      <div className="name-cont">
-        <div className="name">William Sinn</div>
-        <span>Fullstack Software Engineer</span>
+    <section className="main">
+      <div className="landing">
+        <h1>Hi, my name is.</h1>
+        <h2 className="name">William Sinn</h2>
+        <h3>
+          <span>Fullstack Software Engineer</span>
+        </h3>
+        <div>
+          <p>
+            I'm a software engineer based in Boston, MA specializing in building
+            (and occasionally designing) exceptional websites, applications, and
+            everything in between.
+          </p>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
