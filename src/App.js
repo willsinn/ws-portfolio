5b09d96e6def874bd2d24212c2fd39494f51ad36
William Sinn <w.h.sinn@gmail.com>
2020-08-02 08:52:12 -0400

    add css variables, add landingPage & update height

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import AboutPage from "./components/AboutPage";
 import ProjectsPage from "./components/ProjectsPage";
+import LandingPage from "./components/LandingPage";
+
 import "./App.css";
 
 const App = () => {
   return (
     <div className="app">
+      <LandingPage />
       <AboutPage />
       <ProjectsPage />
     </div>
---
+++ src/components/LandingPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div className="landing">
+      <div className="name-cont">
+        <div className="name">William Sinn</div>
+        <span>Fullstack Software Engineer</span>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
