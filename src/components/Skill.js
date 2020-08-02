4c6d246e78008400ac04d30c6963c6d63cd422b3
William Sinn <w.h.sinn@gmail.com>
2020-08-02 11:36:01 -0400

    save branch

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Skill = ({ skill }) => {
   const icnStyle = { color: "var(--txt-yw)" };
-  const txtStyle = { color: "var(--txt-wht)" };
+  const txtStyle = { color: "var(--txt-wht)", marginLeft: "4px" };
   return (
     <li className="skill-item" key={`${skill.name}-id`}>
       <div>
