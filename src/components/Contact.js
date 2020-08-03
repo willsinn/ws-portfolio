cdbaa5089762b942ffa85ecc342c88bfff6b29aa
William Sinn <w.h.sinn@gmail.com>
2020-08-03 17:25:32 -0400

    add polished fnality of the copy email modal

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,45 +3,43 @@ const Contact = ({ contact, orient }) => {
   const [popup, setPopup] = useState(false);
   const [msg, setMsg] = useState(false);
   const [hover, setHover] = useState(false);
-  const emailClipboard = (
-    <div className="popover">
-      <div className="clip-cont col-1">
-        <div className="active-link clip-title col-1">My Email Address</div>
-        <div className="clipboard" onMouseEnter={(e) => renderDirections(e)}>
-          <textarea className="clip-text">{contact.link}</textarea>
-          {msg ? (
-            <div className="popup-info">
-              <div className="triangle-up"></div>Copied!!
-            </div>
-          ) : null}
-          {hover ? (
-            <div className="popup-info">
-              <div className="triangle-up"></div>Copy to clipboard
-            </div>
-          ) : null}
-          <i
-            className="fa fa-clipboard"
-            aria-hidden="true"
-            onClick={(e) => copyToClipboard(e)}
-          ></i>
+  const emailClipboard = () => {
+    return (
+      <div className="popover">
+        <div className="clip-cont col-1">
+          <div className="active-link clip-title col-1">My Email Address</div>
+          <div className="clipboard">
+            <textarea className="clip-text">{contact.link}</textarea>
+            {msg ? (
+              <div className="popup-info">
+                <div className="triangle-up"></div>
+                <span>Copied!!</span>
+              </div>
+            ) : null}
+            {hover ? (
+              <div className="popup-info">
+                <div className="triangle-up"></div>
+                <span>Copy to clipboard</span>
+              </div>
+            ) : null}
+            <i
+              className="fa fa-clipboard"
+              aria-hidden="true"
+              onClick={(e) => copyToClipboard(e)}
+            ></i>
+          </div>
         </div>
       </div>
-    </div>
-  );
-  const renderDirections = (e) => {
-    if (e && !hover) {
-      setHover(!hover);
-      setTimeout(() => setHover(false), 5000);
-    }
+    );
   };
-  console.log(hover);
   const copyToClipboard = (e) => {
     if (e) {
+      setHover(false);
       const elem = e.target.parentElement.firstElementChild;
       elem.select();
       document.execCommand("copy");
       setMsg(!msg);
-      setTimeout(() => setMsg(false), 2000);
+      setTimeout(() => setPopup(false), 2500);
     }
   };
   const renderContact = () => {
@@ -51,7 +49,7 @@ const Contact = ({ contact, orient }) => {
           return (
             <div className="icon">
               <div target="_blank" rel="noopener noreferrer">
-                {popup ? emailClipboard : null}
+                {popup ? <>{emailClipboard()}</> : null}
                 <i
                   className="fa fa-envelope"
                   aria-hidden="true"
@@ -92,6 +90,9 @@ const Contact = ({ contact, orient }) => {
   const handleEmailClick = (e) => {
     if (e) {
       setPopup(!popup);
+      setHover(!hover);
+
+      setTimeout(() => setHover(false), 3000);
     }
   };
   return (
