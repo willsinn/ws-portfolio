39741bf248640eb057a3525fcf4c0bdfca6cf4b9
William Sinn <w.h.sinn@gmail.com>
2020-08-02 21:11:24 -0400

    fix navbar bugs

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import WorkPage from "./components/WorkPage";
 import LandingPage from "./components/LandingPage";
 import Navbar from "./components/Navbar";
 import ContactList from "./components/ContactList";
-import HexagonGrid from "./containers/HexagonGrid";
 
 import "./App.css";
 
 const App = () => {
   return (
     <div className="app">
-      <HexagonGrid />
-
       <div className="sider contact-links">
         <ContactList orient={"vertical"} />
       </div>
