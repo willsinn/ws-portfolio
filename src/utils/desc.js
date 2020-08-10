875a78a8e7d6c0e88da55d1cbf91ebc94687dcbd
William Sinn <w.h.sinn@gmail.com>
2020-08-10 00:17:57 -0400

    update connect desc

--- src/utils/desc.js
+++ src/utils/desc.js
@@ -5,4 +5,4 @@ export const abtMission =
   "My mission is to improve user experience through simplicity and intuitive design while optimizing the product codebase. Using data points to precisely upgrade weakpoints has always been a strong suit of mine and I intend to build on it!";
 
 export const connectOutro =
-  "As a person who believes in collaborative solutions, I'm currently looking to compliment a developer team.";
+  "As a positive, friendly personality who loves collaborating on solutions, I'm currently looking for a challenge.";
